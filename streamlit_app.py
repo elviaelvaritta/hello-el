838d53b962214e625c35ad666aa88d46ec35a33e
elviaelvaritta <elvarita28@gmail.com>
2026-05-22 14:25:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.markdown("*Hello* aku **elvia** ***keren***.")
 st.markdown('''
     :red[Selamat] :orange[datang] :green[di aplikasi] :blue[ini] :violet[semoga]
-    :gray[suka] :rainbow[dengan] and :blue-background[aplikasi ini].''')
+    :gray[suka] :rainbow[dengan]  :blue-background[aplikasi ini].''')
 st.markdown("Here's a bouquet &mdash;\
             :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
 
